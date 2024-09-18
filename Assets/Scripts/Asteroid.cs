e77330b8819bc35b6bb8c1fb6a0fe2ebe8b87e7a
realspork <trentnall0120@gmail.com>
2024-09-17 21:44:40 -0500

    added sprites, spawning, and asteroid movement
    added sprites for everything, an asteroid spawner, and the ability for asteroids to move in a trajectory. Spawner instantiation is null?

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,45 @@
+using System;
+using static System.Console;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+
+    public Sprite[] sprites; 
+    public float size = 1.0f;
+    public float minSize = 0.5f;
+    public float maxSize = 1.5f;
+    public float speed = 50.0f;
+    public float maxLifetime = 30.0f;
+
+
+    private SpriteRenderer _spriteRenderer;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Start()
+    {
+        _spriteRenderer.sprite = sprites[UnityEngine.Random.Range(0, sprites.Length)];
+
+        this.transform.eulerAngles = new Vector3(0.0f, 0.0f, UnityEngine.Random.value * 360.0f);
+        this.transform.localScale = Vector3.one * this.size;
+
+        _rigidbody.mass = this.size;
+    }
+
+    public void SetTrajectory(Vector2 direction)
+    {
+        _rigidbody.AddForce(direction * this.speed);
+
+        Destroy(this.gameObject, this.maxLifetime);
+    }
+
+}
